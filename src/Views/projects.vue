793110d0fe335f123beb96b1a1440f2e0c73d55e
Asanka Gamage <asankagamage1998@gmail.com>
2021-12-09 22:52:41 +0530

    finished roject page 90%

--- src/Views/projects.vue
+++ src/Views/projects.vue
@@ -9,6 +9,16 @@
                 <div>
                      <p class="topics">Education institute management system</p>
                 </div>
+                <div class="phra1">
+                    <p>Shilpa Education management system is our second year 2nd semester <br>
+                     main project of ITP subject. this project was done by 8 group members.<br>
+                      there ,we have used deffernt technologies for frontend and backend.<br>
+                       frontend has done by vuejs and backend has done by laravel.    </p>
+                </div><br>
+                <div class="phra2">
+                     frontend <a href="//github.com/asankagamage98/Shilpa-eims-frontend-employee-">//github.com/asankagamage98/Shilpa-eims-frontend-employee-</a><br>
+                     backend  <a href="https://github.com/asankagamage98/Shilpa-eims-backend-employee-">https://github.com/asankagamage98/Shilpa-eims-backend-employee-</a> 
+                </div>
 
 
             </div>
@@ -23,6 +33,15 @@
                 <div>
                     <p class="topics">UniLife mobile application </p>
                 </div>
+                <div class="phra1">
+                    <p>we have done UniLife mobile application for university students. <br>
+                     it can be managed lectures ,events,exams and students can be generated<br>
+                     their GPA from given gpa calculation.our group has a four members.<br>
+                     we  used java and sqlit  for this app.   </p>
+                </div><br>
+                <div class="phra2">
+                    Github link  <a href="https://github.com/asankagamage98/UniLife.git">https://github.com/asankagamage98/UniLife.git</a><br>
+                 </div>
             </div>
         </div><br>
 
@@ -35,6 +54,14 @@
                 <div>
                     <p class="topics">Re designed NIE website </p>
                 </div>
+                <div class="phra1">
+                    <p>This project is, our theard year first semester project of hci subject.<br> 
+                        then we have redesign National institute of education website.<br>
+                        it has redesigned by adobeXD software. there for particiated four members for build.    </p>
+                </div><br>
+                <div class="phra2">
+                                   Behance link <a href="https://www.behance.net/gallery/128582503/NIE-Website-Redesigned-Prototype">https://www.behance.net/gallery/128582503/NIE-Website-Redesigned-Prototype</a><br>
+                 </div>
             </div>
         </div><br>
 
@@ -44,8 +71,16 @@
                    <img src="../assets/hos.png" class="imghome" alt="">
                </div>
                <div>
-                    <p class="topics"> Hospital management system </p>
+                    <p class="topics"> E-channeling system </p>
                </div>
+               <div class="phra1">
+                    <p>this project is online e-channeling system regarding rivet hospital.<br>
+                         it can be registerd doctors and patients furthermore patient can be booked doctors<br>
+                         we have used html,css and java  for build this project. </p>
+                </div><br>
+                <div class="phra2">
+                                   Github link <a href="https://github.com/asankagamage98/second_year_1stsem_project.git">https://github.com/asankagamage98/second_year_1stsem_project.git</a><br>
+                 </div>
             </div>
         </div><br>
 
@@ -56,6 +91,13 @@
                <div>
                    <p  class="topics">"Prezz" Online presentation system</p>
                </div>
+               <div class="phra1">
+                    <p>this  is our 1st year second semester project. there has been used <br>
+                    several languages that are ,html css and php.five group members joined for build thi project. </p>
+                </div><br>
+                <div class="phra2">
+                                   Github link <a href="https://github.com/asankagamage98/second_sem_project.git">https://github.com/asankagamage98/second_sem_project.git</a><br>
+                 </div>
             </div>
         </div><br>
 
@@ -96,4 +138,16 @@
      margin-top:-150px;
 
  }
+ .phra1 {
+     color: white;
+     position: absolute;
+     left:550px;
+     margin-top:-100px;
+  }
+   .phra2 {
+     color: white;
+     position: absolute;
+     left:200px;
+  
+  }
 </style>
